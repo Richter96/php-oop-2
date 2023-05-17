87a4fbc148948d6b8aeaf9dd79e87ed9af199af9
Riccardo castiglione <castiglione96.rc@gmail.com>
2023-05-17 17:13:46 +0200

    add class monitor

--- index.php
+++ index.php
@@ -20,26 +20,81 @@ class Computer
         $this->keyboard = $keyboard;
         $this->mouse = $mouse;
     }
+    public function getComputerDetail() // ???? quando va utilizzata?
+    {
+        # code...
+    }
 }
 
-class PortableComputer extends Computer
-{
 
+
+class Portable extends Computer
+{
+    //parent:: serve?
     public $battery;
     public $webcam;
     public $microphone;
     public $display;
+
+    function __construct($cpu, $motherboard, $hard_disk, $ram, $gpu, $keyboard, $mouse, string $battery, string $webcam, string $microphone, string $display)
+    {
+        parent::__construct($cpu, $motherboard, $hard_disk, $ram, $gpu, $keyboard, $mouse);
+        $this->battery = $battery;
+        $this->webcam = $webcam;
+        $this->microphone = $microphone;
+        $this->display = $display;
+    }
 }
 
-class DesktopComputer
+class Desktop extends Computer
 {
     public $case;
     public $external_speakers;
     public $monitor;
     public $external_webcam;
+
+    function __construct($cpu, $motherboard, $hard_disk, $ram, $gpu, $keyboard, $mouse, string $case, string $external_speakers, Monitor $monitor, string $external_webcam)
+    {
+        parent::__construct($cpu, $motherboard, $hard_disk, $ram, $gpu, $keyboard, $mouse);
+        $this->case = $case;
+        $this->external_speakers = $external_speakers;
+        $this->monitor = $monitor;
+        $this->external_webcam = $external_webcam;
+    }
 }
 
-$computer_portatile = new PortableComputer(processore_x, schedaMadre_x, hard_disk_x, ram_x, schedaVideo_X, tastiera_x, mouse_x,)
+class Monitor
+{
+    public $type;
+    public $size;
+    public $herz;
+    public $resolution;
+
+    public function __construct($type, $size, $herz, $resolution)
+    {
+        $this->type = $type;
+        $this->size = $size;
+        $this->herz = $herz;
+        $this->resolution = $resolution;
+    }
+}
+
+$computer = new Computer('processore_x', 'schedaMadre_x', 'hard_disk_x', 'ram_x', 'schedaVideo_X', 'tastiera_x', 'mouse_x');
+var_dump($computer);
+
+$computer1 = new Portable('processore_y', 'schedaMadre_y', 'hard_disk_y', 'ram_y', 'schedaVideo_y', 'tastiera_y', 'mouse_y', 'batteria_y', 'webcam_y', 'microfono_y', 'display_y');
+
+$computer2 = new Desktop('processore_ß', 'schedaMadre_ß', 'hard_disk_ß', 'ram_ß', 'schedaVideo_ß', 'tastiera_ß', 'mouse_ß', 'case_x', 'speacker_ß', new Monitor('led', '120', '120hz', '3024px'), 'webcams_');
+
+$computers = [
+    $computer1,
+    $computer2
+];
+
+var_dump($computers);
+
+
+
 
 ?>
 
@@ -89,7 +144,7 @@ $computer_portatile = new PortableComputer(processore_x, schedaMadre_x, hard_dis
     <main>
 
     </main>
-
+    <div class="container"></div>
 
 
 
