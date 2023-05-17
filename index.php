54813cbe8a95da5f26c67afcee174835af828883
Riccardo castiglione <castiglione96.rc@gmail.com>
2023-05-17 15:39:40 +0200

    create class

--- index.php
+++ index.php
@@ -1,6 +1,59 @@
 <?php
 
+class Computer
+{
+    public $cpu;
+    public $motherboard;
+    public $hard_disk;
+    public $ram;
+    public $gpu;
+    public $keyboard;
+    public $mouse;
+
+    public function __construct($cpu, $motherboard, $hard_disk, $ram, $gpu, $keyboard, $mouse)
+    {
+        $this->cpu = $cpu;
+        $this->motherboard = $motherboard;
+        $this->hard_disk = $hard_disk;
+        $this->ram = $ram;
+        $this->gpu = $gpu;
+        $this->keyboard = $keyboard;
+        $this->mouse = $mouse;
+    }
+}
+
+class PortableComputer extends Computer
+{
+    public $battery;
+    public $webcam;
+    public $microphone;
+    public $display;
+}
+
+class DesktopComputer
+{
+    public $case;
+    public $external_speakers;
+    public $monitor;
+    public $external_webcam;
+}
+
 ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 <!doctype html>
 <html lang="en">
@@ -21,9 +74,25 @@
     <header>
         <!-- place navbar here -->
     </header>
+
+
+
+
+
+
+
+
+
     <main>
 
     </main>
+
+
+
+
+
+
+
     <footer>
         <!-- place footer here -->
     </footer>
