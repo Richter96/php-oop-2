2e2a135df139f53fb1d75c3f5aff2713217a2e6e
Riccardo castiglione <castiglione96.rc@gmail.com>
2023-05-18 20:30:26 +0200

    add function trait

--- trails/Monitor.php
+++ trails/Monitor.php
@@ -1,22 +1,42 @@
 <?php
 
 
-trait display_set
+trait Monitor
 {
-    public $type;
-    public $size;
-    public $herz;
-    public $resolution;
+    private $type_display;
+    private $size;
+    private $herz;
+    private $resolution;
 
-    public function __construct($type, $size, $herz, $resolution)
+    /* public function __construct($type, $size, $herz, $resolution)
     {
         $this->type = $type;
         $this->size = $size;
         $this->herz = $herz;
         $this->resolution = $resolution;
+    } */
+
+    public function getMonitor_as_array()
+    {
+        $array = [
+            $this->type_display,
+            $this->size,
+            $this->herz,
+            $this->resolution,
+        ];
+        return $array;
     }
 
-    public function setDisplaySet()
+    public function setMonitor(string $_type_display, string $_size, string $_herz, string $_resolution)
     {
+        $this->type_display = $_type_display;
+        $this->size = $_size;
+        $this->herz = $_herz;
+        $this->resolution = $_resolution;
+    }
+
+    public function getTypeDisplay()
+    {
+        return $this->type_display;
     }
 }
