6afc749924912771b165e689cfaa43e484845d1f
Riccardo castiglione <castiglione96.rc@gmail.com>
2023-05-18 20:26:44 +0200

    add vardump for check

--- Database/db.php
+++ Database/db.php
@@ -6,4 +6,12 @@ $computers = [
 
 
 $computers[0]->setType();
+$computers[0]->setMonitor('retina', '60"', '120hrz', '3024px');
 $computers[1]->setType();
+$computers[1]->setMonitor('retina', '60"', '120hrz', '3024px');
+/* 
+check funzionamento monitor
+var_dump($computers);
+
+var_dump($computers[0]->getMonitor_as_array());
+ */
